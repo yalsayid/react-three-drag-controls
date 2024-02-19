f26161436815a47320a10a0ab61255255bfcf274
Yousef Alsayid <15058134+yalsayid@users.noreply.github.com>
2024-02-19 16:12:17 -0500

    Updated demo to add leva controls for axis lock, as well as design/styling updated.

--- src/App.tsx
+++ src/App.tsx
@@ -4,60 +4,78 @@ import {
   GizmoViewport,
   OrbitControls,
   Center,
-  SoftShadows,
+  Grid,
+  Environment,
 } from "@react-three/drei";
+import { useControls } from "leva";
 import { DragControls } from "./DragControls";
 
 export default function App() {
+  const { axisLock } = useControls({
+    axisLock: {
+      label: "Axis Lock",
+      options: {
+        None: undefined,
+        X: "x",
+        Y: "y",
+        Z: "z",
+      },
+      value: undefined,
+    },
+  }) as {
+    axisLock: "x" | "y" | "z" | undefined;
+  };
+
   return (
-    <Canvas shadows camera={{ position: [-10, 10, 10], fov: 20 }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight
-        castShadow
-        position={[2.5, 5, 5]}
-        intensity={1.5}
-        shadow-mapSize={[1024, 1024]}
-      >
-        <orthographicCamera
-          attach="shadow-camera"
-          args={[-5, 5, 5, -5, 1, 50]}
-        />
-      </directionalLight>
-
-      <SoftShadows />
-
-      <mesh scale={20} receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry />
-        <shadowMaterial transparent opacity={0.5} />
-      </mesh>
-
-      <DragControls>
-        <mesh castShadow receiveShadow position={[-1, 0.5, 1]}>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial />
-        </mesh>
-      </DragControls>
-
-      <DragControls>
-        <mesh position={[0.75, 0.5, 1]} castShadow receiveShadow>
-          <boxGeometry args={[0.5, 1, 0.5]} />
-          <meshStandardMaterial />
-        </mesh>
-      </DragControls>
-
-      <DragControls>
-        <Center top position={[1.5, 0, 0]}>
-          <mesh castShadow receiveShadow>
+    <Canvas shadows camera={{ position: [10, 12, 12], fov: 25 }}>
+      <DragControls axisLock={axisLock}>
+        <Center top>
+          <mesh castShadow receiveShadow rotation={[-0.63, 0, 0]} scale={2}>
             <dodecahedronGeometry args={[0.5]} />
-            <meshStandardMaterial color="white" />
+            <meshStandardMaterial color="#a78bfa" />
+          </mesh>
+        </Center>
+      </DragControls>
+      <DragControls axisLock={axisLock}>
+        <Center top position={[-1, 0, 2]}>
+          <mesh castShadow>
+            <sphereGeometry args={[0.5, 64, 64]} />
+            <meshStandardMaterial color="#a78bfa" />
+          </mesh>
+        </Center>
+      </DragControls>
+      <DragControls axisLock={axisLock}>
+        <Center top position={[2, 0, 1]}>
+          <mesh castShadow rotation={[0, Math.PI / 4, 0]}>
+            <boxGeometry args={[0.7, 0.7, 0.7]} />
+            <meshStandardMaterial color="#a78bfa" />
           </mesh>
         </Center>
       </DragControls>
 
-      <GizmoHelper alignment="bottom-right" margin={[100, 100]}>
-        <GizmoViewport labelColor="white" axisHeadScale={1} />
-      </GizmoHelper>
+      <Grid
+        position={[0, -0.01, 0]}
+        receiveShadow
+        args={[10.5, 10.5]}
+        infiniteGrid
+        fadeDistance={50}
+        fadeStrength={1}
+        sectionColor={"#a78bfa"}
+        sectionSize={4}
+        sectionThickness={1.5}
+        cellColor={"#f5f3ff"}
+        cellSize={1}
+        cellThickness={1}
+      />
+
       <OrbitControls makeDefault />
+      <Environment preset="city" />
+      <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+        <GizmoViewport
+          axisColors={["#9d4b4b", "#2f7f4f", "#3b5b9d"]}
+          labelColor="white"
+        />
+      </GizmoHelper>
     </Canvas>
   );
 }
